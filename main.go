a0a1f7d747dc23910f785533a0e6e8abfb27f77f
barnettZQG <zengqg@goodrain.com>
2020-09-04 17:10:06 +0800

    change package name

--- main.go
+++ main.go
@@ -11,10 +11,10 @@ import (
 
 	"github.com/codegangsta/cli"
 
-	"github.com/yudai/gotty/backend/localcommand"
-	"github.com/yudai/gotty/pkg/homedir"
-	"github.com/yudai/gotty/server"
-	"github.com/yudai/gotty/utils"
+	"github.com/barnettZQG/gotty/backend/localcommand"
+	"github.com/barnettZQG/gotty/pkg/homedir"
+	"github.com/barnettZQG/gotty/server"
+	"github.com/barnettZQG/gotty/utils"
 )
 
 func main() {
--- server/server.go
+++ server/server.go
@@ -18,9 +18,9 @@ import (
 	"github.com/gorilla/websocket"
 	"github.com/pkg/errors"
 
-	"github.com/yudai/gotty/pkg/homedir"
-	"github.com/yudai/gotty/pkg/randomstring"
-	"github.com/yudai/gotty/webtty"
+	"github.com/barnettZQG/gotty/pkg/homedir"
+	"github.com/barnettZQG/gotty/pkg/randomstring"
+	"github.com/barnettZQG/gotty/webtty"
 )
 
 // Server provides a webtty HTTP endpoint.
